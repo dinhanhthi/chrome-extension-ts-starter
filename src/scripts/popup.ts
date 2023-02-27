d3d9d914bd13fdc7dcf795feacc867153ad222bd
Thi <thi@ideta.io>
2023-02-27 11:29:20 +0100

    Use webextension-polyfill -> "browser" instead of "chrome"

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,1 +1,12 @@
-import '../styles/style.scss';
+import browser from 'webextension-polyfill'
+
+import '../styles/style.scss'
+
+// An example of async send message
+browser.runtime.sendMessage({ type: 'hello' }).then(response => {
+  console.log('+++response', response)
+})
+
+browser.runtime.sendMessage({ type: 'login' }).then(response => {
+  console.log('+++response', response)
+})
